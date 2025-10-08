439682046b7aecd89997b159b04332c61cad1750
harsh-sh-1910 <harshsh1910@gmail.com>
2025-10-08 15:12:03 +0530

    fixed folder naming

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Main from "./components/Main/Main";
 import Solutions from "./components/Solutions/Solutions";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Input from "./components/Input/Input";
-import UseContextProvider from "./Usecontext/UseContextProvider";
+import UseContextProvider from "./UseContext/UseContextProvider";
 
 const App = () => {
   return (
